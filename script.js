00ef2a4195b75795103a424980c0bc4e88845154
AkhilAkhilesh <120119311+AkhilAkhilesh@users.noreply.github.com>
2026-04-07 16:25:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const ele = document.getElementsByClassName('square');
+		Array.from(ele).forEach(element => {
+			element.addEventListener('mouseover', (e) => {
+				for(let i=0; i<ele.length; i++){
+					if(!(ele[i] === e.target)){
+						ele[i].style.backgroundColor = '#6F4E37';
+					}
+				}
+				return;
+			});
+
+			element.addEventListener('mouseout', () => {
+				for(let i=0; i<ele.length; i++){
+					ele[i].style.backgroundColor = '';
+				}
+			});
+		});
